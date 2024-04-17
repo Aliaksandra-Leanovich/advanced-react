674b36ec30f23233dfdda3eee6c8944382f1cfbe
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2024-04-17 15:45:29 +0300

    add:controlled component flows

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { UncontrolledFlow } from "./components/uncontrolled-flow";
+import { ControlledFlow } from "./components/controlled-flow";
 
 const StepOne = ({ next }) => {
   return (
@@ -12,32 +14,45 @@ const StepTwo = ({ next }) => {
   return (
     <>
       <h1>Step #2: Enter your age</h1>
-      <button onClick={() => next({ age: 23 })}>Next</button>
+      <button onClick={() => next({ age: 30 })}>Next</button>
     </>
   );
 };
 const StepThree = ({ next }) => {
   return (
     <>
-      <h1>Step #3: Enter your country</h1>
+      <h1>Step #3: You qualify!</h1>
+      <button onClick={() => next({})}>Next</button>
+    </>
+  );
+};
+
+const StepFour = ({ next }) => {
+  return (
+    <>
+      <h1>Step #4: Enter your country</h1>
       <button onClick={() => next({ country: "Poland" })}>Next</button>
     </>
   );
 };
 
 function App() {
+  const [data, setData] = useState({});
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const next = (dataFromStep) => {
+    setData(dataFromStep);
+    setCurrentStepIndex(currentStepIndex + 1);
+  };
+
   return (
     <>
-      <UncontrolledFlow
-        onDone={(data) => {
-          console.log(data);
-          alert("Onboarding Flow Done!");
-        }}
-      >
+      <ControlledFlow currentStepIndex={currentStepIndex} onNext={next}>
         <StepOne />
         <StepTwo />
-        <StepThree />
-      </UncontrolledFlow>
+        {data.age > 25 && <StepThree />}
+        <StepFour />
+      </ControlledFlow>
     </>
   );
 }
