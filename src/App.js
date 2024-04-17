ea2f4a23d1fa5fe9afcb42ef539f2e81a6ce2f98
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2024-04-17 15:08:46 +0300

    add: uncontrolled component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import axios from "axios";
-import { BookInfo } from "./components/book-info";
-import { DataSource } from "./components/data-source";
-import { UserInfo } from "./components/user-info";
-
-const fetchData = async (url) => {
-  const response = await axios.get(url);
-  return response.data;
-};
-
-const getDataFromLocalStorage = (key) => () => {
-  return localStorage.getItem(key);
-};
-
-const Message = ({ msg }) => <h1>{msg}</h1>;
+import { UncontrolledForm } from "./components/uncontrolled-form";
 
 function App() {
   return (
     <>
-      <DataSource getData={() => fetchData("/users/1")} resourceName={"user"}>
-        <UserInfo />
-      </DataSource>
-
-      <DataSource
-        getData={() => getDataFromLocalStorage("test")}
-        resourceName={"msg"}
-      >
-        <Message />
-      </DataSource>
+      <UncontrolledForm />
     </>
   );
 }
