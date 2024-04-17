c2ad4c627e2fc69250750287c5ce607ca2a82c6c
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2024-04-17 12:57:23 +0300

    add: loader comp for CurrentUser data

--- src/components/current-user-loader.jsx
+++ src/components/current-user-loader.jsx
@@ -15,7 +15,10 @@ export const CurrentUserLoader = ({ children }) => {
     <>
       {React.Children.map(children, (child) => {
         if (React.isValidElement(child)) {
+          // Check if the child element is a valid React element
+
           return React.cloneElement(child, { user });
+          // Clone the child element and pass the 'user' prop to it
         }
 
         return child;
