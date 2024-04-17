cee303995098e9f52c238a4ecfbffd42243de8ca
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2024-04-17 12:01:31 +0300

    feat: add list

---
+++ src/components/books/LargeListItem.jsx
@@ -0,0 +1,13 @@
+export const LargeBookListItem = ({ book }) => {
+  const { name, price, title, pages } = book;
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <p>{price}</p>
+      <h2>Title:</h2>
+      <p>{title}</p>
+      <p># of Pages: {pages}</p>
+    </>
+  );
+};
