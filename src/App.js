41290507c56cc82a2e81170699f220b56b2b01ff
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2024-04-17 12:53:41 +0300

    add: loader for user Data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { CurrentUserLoader } from "./components/current-user-loader";
+import { UserInfo } from "./components/user-info";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <CurrentUserLoader>
+        <UserInfo />
+      </CurrentUserLoader>
+    </>
+  );
 }
 
 export default App;
