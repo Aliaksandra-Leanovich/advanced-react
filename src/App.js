eebfbd2fd63ae2d12f4bf63c9db08d2a1f3f7d1b
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2024-04-22 11:31:01 +0300

    feat: data loading with hoc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { checkProps } from "./components/check-props";
+import { includeUser } from "./components/include-user";
+import { UserInfo } from "./components/user-info";
+
+const UserInfoWithUser = includeUser(UserInfo, "2");
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <UserInfoWithUser />
+    </>
+  );
 }
 
 export default App;
