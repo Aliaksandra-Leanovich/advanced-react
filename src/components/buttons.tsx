61f4c5903065fff7d2cad27bf0cde99ced336217
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2024-04-23 22:39:25 +0300

    feat: add context api

---
+++ src/components/buttons.tsx
@@ -0,0 +1,23 @@
+import { useDispatchContext } from "./cart-context";
+
+const Buttons = () => {
+  const dispatch = useDispatchContext();
+  return (
+    <div className="buttons">
+      <button
+        className="button"
+        onClick={() => dispatch({ type: "DECREMENT" })}
+      >
+        ➖
+      </button>
+      <button
+        className="button"
+        onClick={() => dispatch({ type: "INCREMENT" })}
+      >
+        ➕
+      </button>
+    </div>
+  );
+};
+
+export default Buttons;
---
+++ src/components/display.tsx
@@ -0,0 +1,8 @@
+import { useStateContext } from "./cart-context";
+
+const Display = () => {
+  const { count } = useStateContext();
+  return <span className="span">{count}</span>;
+};
+
+export default Display;
